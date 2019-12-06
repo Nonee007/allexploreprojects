351a6885400f3382b2e3248869ccd93bc4e92704
Nonee007 <iqif2012@gmail.com>
2019-12-06 16:46:14 -0600

    javaAss1

---
+++ src/hw.java
@@ -0,0 +1,11 @@
+//1.	Set up a project in Eclipse and print out “Hello, my name is ___” in the console
+//2.	Create a *.java file using notepad of the same program (part 1) and run it using CMD
+
+
+public class hw {
+public static void main(String[] args) {
+	System.out.println("Hello, my name is whatever!");
+}
+}
+
+
